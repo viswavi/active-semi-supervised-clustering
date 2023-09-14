5de34a4724cbb127ad8edb8014ec3ba9437f58a2
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2023-09-14 11:16:37 -0400

    save

--- active_semi_clustering/active/pairwise_constraints/__init__.py
+++ active_semi_clustering/active/pairwise_constraints/__init__.py
@@ -3,7 +3,7 @@ from .min_max import MinMax, SimilarityFinder
 from .min_max_finetune import MinMaxFinetune
 from .npu import NPU
 from .example_oracle import ExampleOracle
-from .gpt3_pc_oracle import GPT3Oracle
+from .gpt3_pc_oracle import GPT3Oracle, construct_pairwise_oracle_single_example
 from .gpt3_comparative_oracle import GPT3ComparativeOracle
 from .label_based_selector import LabelBasedSelector
 from .distance_based_selector import DistanceBasedSelector
--- active_semi_clustering/active/pairwise_constraints/gpt3_pc_oracle.py
+++ active_semi_clustering/active/pairwise_constraints/gpt3_pc_oracle.py
@@ -48,17 +48,53 @@ def call_chatgpt(prompt, num_predictions, temperature=1.0, max_tokens=1, timeout
     )
     return response
 
+
+
+def construct_pairwise_oracle_single_example(document_i, document_j, label, dataset_name, prompt_suffix = None, text_type = "None", add_label=True):
+    if prompt_suffix is None:
+        if dataset_name == "OPIEC59k":
+            prompt_suffix = "link to the same entity's article on Wikipedia? "
+        elif dataset_name == "reverb45k":
+            prompt_suffix = "link to the same entity on a knowledge graph like Freebase? "
+        elif dataset_name == "tweet":
+            prompt_suffix = "discuss the same topic? "
+        elif dataset_name == "clinc":
+            prompt_suffix = "express the same general intent? "
+        elif dataset_name == "bank77":
+            prompt_suffix = "express the same general intent? "
+        else:
+            raise ValueError(f"Dataset {dataset_name} not supported. Set a prompt_suffix")
+
+    if text_type is None:
+        if dataset_name == "OPIEC59k" or dataset_name == "reverb45k":
+            text_type = "Entity"
+        else:
+            if dataset_name == "tweet":
+                text_type = "Tweet"
+            elif dataset_name == "clinc":
+                text_type = "Utterance"
+            elif dataset_name == "bank77":
+                text_type = "Utterance"
+            else:
+                raise ValueError(f"Dataset {dataset_name} not supported.")
+    template_prefix = f"""{text_type} #1: {document_i}
+{text_type} #2: {document_j}
+
+Given this context, do {text_type.lower()} #1 and {text_type.lower()} #2 likely {prompt_suffix}"""
+
+    if add_label:
+        full_example = template_prefix + label
+    else:
+        full_example = template_prefix
+    return full_example
+
 class GPT3Oracle:
-    def __init__(self, X, labels, dataset_name, split=None, max_queries_cnt=2500, num_predictions=5, side_information=None, read_only=False):
-        self.labels = labels
+    def __init__(self, X, prompt, cache_file_name=None, max_queries_cnt=2500, num_predictions=5, read_only=False):
         self.queries_cnt = 0
         self.max_queries_cnt = max_queries_cnt
         self.num_predictions = num_predictions
 
-        self.side_information = side_information
-        self.cache_dir = "/projects/ogma1/vijayv/okb-canonicalization/clustering/file/gpt3_cache"
-        self.dataset_name = dataset_name
-        self.cache_file = os.path.join(self.cache_dir, f"{dataset_name}_pairwise_constraint_cache.jsonl")
+        self.cache_file = cache_file_name
         if os.path.exists(self.cache_file):
             self.cache_rows = list(jsonlines.open(self.cache_file))
         else:
@@ -71,166 +107,12 @@ class GPT3Oracle:
         self.NUM_RETRIES = 2
         self.read_only = read_only
 
-        if not isinstance(self.side_information, list):
-            side_info = self.side_information.side_info
-            self.sentence_unprocessing_mapping_file = os.path.join(self.cache_dir, f"{dataset_name}_{split}_sentence_unprocessing_map.json")
-            sentence_unprocessing_mapping = json.load(open(self.sentence_unprocessing_mapping_file))
-        selected_sentences = []
-        ents = []
-
-        for i in range(len(X)):
-            if isinstance(self.side_information, list):
-                selected_sentences.append([self.side_information[i]])
-                ents.append(self.side_information[i])
-            else:
-                ents.append(side_info.id2ent[i])
-                entity_sentence_idxs = side_info.ent_id2sentence_list[i]
-                unprocessed_sentences = [sentence_unprocessing_mapping[side_info.sentence_List[j]] for j in entity_sentence_idxs]
-                entity_sentences = self.process_sentence_punctuation(unprocessed_sentences)
-                entity_sentences_dedup = list(set(entity_sentences))
-
-                '''
-                Choose longest sentence under 306 characers, as in
-                https://github.com/Yang233666/cmvc/blob/6e752b1aa5db7ff99eb2fa73476e392a00b0b89a/Context_view.py#L98
-                '''
-                longest_sentences = sorted([s for s in entity_sentences_dedup if len(s) < 599], key=len)
-                selected_sentences.append(list(set(longest_sentences[:3])))
-
-        self.ents = ents
-        self.selected_sentences = selected_sentences
-
         self.gpt3_pairwise_labels = {}
         for row in self.cache_rows:
             sorted_pair_list = sorted([row["entity1"], row["entity2"]])
             self.gpt3_pairwise_labels[tuple(sorted_pair_list)] = row["labels"]
 
-
-    def process_sentence_punctuation(self, sentences):
-        processed_sentence_set = []
-        for s in sentences:
-            processed_sentence_set.append(s.replace("-LRB-", "(").replace("-RRB-", ")"))
-        return processed_sentence_set
-
-
-    def construct_single_example(self, i, j, add_label=True):
-        if self.dataset_name == "OPIEC59k":
-            prompt_suffix = "link to the same entity's article on Wikipedia? "
-        elif self.dataset_name == "reverb45k":
-            prompt_suffix = "link to the same entity on a knowledge graph like Freebase? "
-        elif self.dataset_name == "tweet":
-            prompt_suffix = "discuss the same topic? "
-        elif self.dataset_name == "clinc":
-            prompt_suffix = "express the same general intent? "
-        elif self.dataset_name == "bank77":
-            prompt_suffix = "express the same general intent? "
-        else:
-            raise ValueError(f"Dataset {self.dataset_name} not supported.")
-
-        if self.dataset_name == "OPIEC59k" or self.dataset_name == "reverb45k":
-            context_labels = ["a", "b", "c", "d"]
-            context_1 = "\n".join([context_labels[ind] + ") " + '"' + sent + '"' for ind, sent in enumerate(self.selected_sentences[i])])
-            context_2 = "\n".join([context_labels[ind] + ") " + '"' + sent + '"' for ind, sent in enumerate(self.selected_sentences[j])])
-            template_prefix = f"""1) {self.ents[i]}
-
-Context Sentences:\n{context_1}
-
-2) {self.ents[j]}
-
-Context Sentence:\n{context_2}
-
-Given this context, would {self.ents[i]} and {self.ents[j]} likely {prompt_suffix}"""
-        else:
-            if self.dataset_name == "tweet":
-                text_type = "Tweet"
-            elif self.dataset_name == "clinc":
-                text_type = "Utterance"
-            elif self.dataset_name == "bank77":
-                text_type = "Utterance"
-            else:
-                raise ValueError(f"Dataset {self.dataset_name} not supported.")
-            template_prefix = f"""{text_type} #1: {self.selected_sentences[i][0]}
-{text_type} #2: {self.selected_sentences[j][0]}
-
-Given this context, do these two {text_type.lower()}s likely {prompt_suffix}"""
-            context_1 = self.selected_sentences[i][0]
-            context_2 = self.selected_sentences[j][0]
-
-        if add_label:
-            if self.labels[i] == self.labels[j]:
-                label = "Yes"
-            else:
-                label = "No"
-            full_example = template_prefix + label
-            return full_example
-        else:
-            return template_prefix, context_1, context_2
-
-    def construct_pairwise_oracle_prompt(self, i, j):
-        if isinstance(self.side_information, list):
-            side_info = None
-        else:
-            side_info = self.side_information.side_info
-        if self.dataset_name == "OPIEC59k":
-            instruction = """You are tasked with clustering entity strings based on whether they refer to the same Wikipedia article. To do this, you will be given pairs of entity names and asked if their anchor text, if used separately to link to a Wikipedia article, is likely referring to the same article. Entity names may be truncated, abbreviated, or ambiguous.
-
-To help you make this determination, you will be given up to three context sentences from Wikipedia where the entity is used as anchor text for a hyperlink. Amongst each set of examples for a given entity, the entity for all three sentences is a link to the same article on Wikipedia. Based on these examples, you will decide whether the first entity and the second entity listed would likely link to the same Wikipedia article if used as separate anchor text.
-
-Please note that the context sentences may not be representative of the entity's typical usage, but should aid in resolving the ambiguity of entities that have similar or overlapping meanings.
-
-To avoid subjective decisions, the decision should be based on a strict set of criteria, such as whether the entities will generally be used in the same contexts, whether the context sentences mention the same topic, and whether the entities have the same domain and scope of meaning.
-
-Your task will be considered successful if the entities are clustered into groups that consistently refer to the same Wikipedia articles."""
-            example_1 = self.construct_single_example(side_info.ent2id["B.A"], side_info.ent2id["M.D."])
-            example_2 = self.construct_single_example(side_info.ent2id["B.A"], side_info.ent2id["bachelor"])
-            example_3 = self.construct_single_example(side_info.ent2id["Duke of York"], side_info.ent2id["Frederick"])
-            example_4 = self.construct_single_example(side_info.ent2id["Academy Award"], side_info.ent2id["Best Actor in Supporting Role"])
-            prefix = "\n\n".join([example_1, example_2, example_3, example_4])
-        elif self.dataset_name == "reverb45k":
-            instruction = """You are tasked with clustering entity strings based on whether they link to the same entity on the Freebase knowledge graph. To do this, you will be given pairs of entity names and asked if these strings, if linked to a knowledge graph, are likely referring to the same entity (e.g. a concept, person, or organization). Entity names may be truncated, abbreviated, or ambiguous.
-
-To help you make this determination, you will be given up to three context sentences from the internet that mention an entity. Amongst each set of examples for a given entity, assume that the entity mentioned in all three context sentences links refers to the same object. Based on these examples, you will decide whether the first entity and the second entity listed are likely to link to the *same* knowledge graph entity.
-
-Please note that the context sentences may not be representative of the entity's typical usage, but should aid in resolving the ambiguity of entities that have similar or overlapping meanings.
-
-To avoid subjective decisions, the decision should be based on a strict set of criteria, such as whether the entities will generally be used in the same contexts, whether the entities likely refer to the same person or organization, whether the context sentences mention the same topic, and whether the entities have the same domain and scope of meaning.
-
-Your task will be considered successful if the entities are clustered into groups that consistently link to the same knowledge graph node."""
-            example_1 = self.construct_single_example(side_info.ent2id["Hannibal"], side_info.ent2id["Hannibal Barca"])
-            example_2 = self.construct_single_example(side_info.ent2id["Lutheran Church"], side_info.ent2id["Church"])
-            example_3 = self.construct_single_example(side_info.ent2id["Grove Art Online"], side_info.ent2id["Oxford Art Online"])
-            example_4 = self.construct_single_example(side_info.ent2id["Charlie Williams"], side_info.ent2id["Williams"])
-            prefix = "\n\n".join([example_1, example_2, example_3, example_4])
-        elif self.dataset_name == "tweet":
-            instruction = """You are tasked with clustering tweets based on whether they discuss the same topic. To do this, you will be given pairs of (stopword-removed) tweets and asked if they discuss the same topic. To avoid subjective decisions, the decision should be based on a strict set of criteria, such as whether the tweets explicitly mention the same topic or whether they reflect the same contexts.
-
-Your task will be considered successful if the tweets are clustered into groups that consistently discuss the same topic."""
-            example_1 = self.construct_single_example(0, 563)
-            example_2 = self.construct_single_example(4, 187)
-            example_3 = self.construct_single_example(2135, 1218)
-            example_4 = self.construct_single_example(2471, 1218)
-            prefix = "\n\n".join([example_1, example_2, example_3, example_4])
-        elif self.dataset_name == "clinc":
-            instruction = """You are tasked with clustering queries for a task-oriented dialog system based on whether they express the same general user intent. To do this, you will be given pairs of user queries and asked if they express the same general user need or intent.
-
-Your task will be considered successful if the queries are clustered into groups that consistently express the same general intent."""
-            example_1 = self.construct_single_example(1, 2)
-            example_2 = self.construct_single_example(70, 700)
-            example_3 = self.construct_single_example(1525, 1527)
-            example_4 = self.construct_single_example(1500, 1000)
-            prefix = "\n\n".join([example_1, example_2, example_3, example_4])
-        elif self.dataset_name == "bank77":
-            instruction = """You are tasked with clustering queries for a online banking system based on whether they express the same general user intent. To do this, you will be given pairs of user queries and asked if they express the same general user need or intent.
-
-Your task will be considered successful if the queries are clustered into groups that consistently express the same general intent."""
-            example_1 = self.construct_single_example(0, 1)
-            example_2 = self.construct_single_example(1990, 2001)
-            example_3 = self.construct_single_example(2010, 2001)
-            example_4 = self.construct_single_example(2900, 3000)
-            prefix = "\n\n".join([example_1, example_2, example_3, example_4])
-        else:
-            raise NotImplementedError
-        filled_template, context_1, context_2 = self.construct_single_example(i, j, add_label = False)
-        return "\n\n".join([instruction, prefix, filled_template]), context_1, context_2
+        self.prompt = prompt
 
     @staticmethod
     def filter_high_entropy_predictions(pair_labels, majority_class_threshold=0.999999):
@@ -253,6 +135,10 @@ Your task will be considered successful if the queries are clustered into groups
 
             if  sorted_pair in self.gpt3_pairwise_labels:
                 return self.filter_high_entropy_predictions(self.gpt3_pairwise_labels[sorted_pair])
+
+
+            prompt = self.prompt
+            prompt_to_be_completed = prompt + 
 
             prompt, context1, context2 = self.construct_pairwise_oracle_prompt(i, j)
             print("PROMPT:\n" + prompt)
